2544b0548de354dd12a20f079fe9dfd7ca7c2e70
Devansh Rautela <maildevanshrautela@gmail.com>
2022-10-09 17:45:54 +0530

    commit

---
+++ Errors.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int mian()
+{
+    float true_value, absolute_value;
+
+    printf("Enter True value and Absolute value\n");
+    scanf("%f %f", &true_value, &absolute_value);
+
+    float absolute_error, relative_error, percentage_error;
+
+    absolute_error = fabs(true_value - absolute_value);
+    relative_error = absolute_error / true_value;
+    percentage_error = relative_error * 100;
+
+    printf("Absolute Error = %f \n Relative Error = %f \n Percentage Error = %f", absolute_error, relative_error, percentage_error);
+}
